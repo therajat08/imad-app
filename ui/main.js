ce391f753376e1549eea2fb6e0354270d2aa0fdc
therajat08 <therajat08@users.noreply.github.com>
2018-02-20 19:26:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //addin new text to id = "okay"
 var element = document.getElementById('okay');
 element.innerHTML = 'now this is new';
+
+//making the image move on click
+var img = document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+}; 
