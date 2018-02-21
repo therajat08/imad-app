e815164cea01a38016eb5879b82e9ff11a73d034
therajat08 <therajat08@users.noreply.github.com>
2018-02-21 12:06:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML = 'now this is new';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight (){
-    marginLeft = marginLeft + 5;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
