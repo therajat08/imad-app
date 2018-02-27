e329cc0d90db042484f13dcf409e4bb8eb56e111
therajat08 <therajat08@users.noreply.github.com>
2018-02-27 12:41:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ var config = {
     password: process.env.DB_PASSWORD
 };
 
+var pool = new Pool('config');
+app.get('test/-db',function(req,res){
+    
+    pool.query('SELECT * FROM test', function(err,result){
+        if (err) {
+            res.status(500).send(err.toString());
+        } else{
+            res.send(JSON.stringfy(result));
+        }
+    });
+});
+    
+
 var app = express();
 app.use(morgan('combined'));
 
